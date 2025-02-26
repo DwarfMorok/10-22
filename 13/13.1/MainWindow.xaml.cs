634914058d6aee6c149213b7915c3e3509cb07ba
DwarfMorok <162573539+DwarfMorok@users.noreply.github.com>
2025-02-26 17:38:45 +0300

    Add files via upload

---
+++ 13/13.1/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace VisualStatesApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
