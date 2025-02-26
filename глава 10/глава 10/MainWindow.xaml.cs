ae4064f06fd739abfe13bb359145c9c2ebdf2e88
DwarfMorok <162573539+DwarfMorok@users.noreply.github.com>
2025-02-26 13:26:54 +0300

    Add files via upload

---
+++ глава 10/глава 10/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _model = "Nexus X5";
+        private string _manufacturer = "LG";
+        private int _price = 25000;
+        private DateTime _currentDateTime = DateTime.Now;
+
+        public string Model
+        {
+            get => _model;
+            set { _model = value; OnPropertyChanged(nameof(Model)); }
+        }
+
+        public string Manufacturer
+        {
+            get => _manufacturer;
+            set { _manufacturer = value; OnPropertyChanged(nameof(Manufacturer)); }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set { _price = value; OnPropertyChanged(nameof(Price)); }
+        }
+
+        public DateTime CurrentDateTime
+        {
+            get => _currentDateTime;
+            set { _currentDateTime = value; OnPropertyChanged(nameof(CurrentDateTime)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Price += 1000; // Пример изменения
+            CurrentDateTime = DateTime.Now; // Обновление даты и времени
+        }
+    }
+
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("dd MMMM yyyy HH:mm:ss");
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
