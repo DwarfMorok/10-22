24d38520a7ee49eaaa0b298671f6c24d14a92fbd
DwarfMorok <162573539+DwarfMorok@users.noreply.github.com>
2025-02-28 01:12:28 +0300

    Add files via upload

---
+++ 17/WPF17/Window4.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF17
+{
+    /// <summary>
+    /// Логика взаимодействия для Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+        }
+        private void ModelUIElement3D_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Произошло нажатие");
+        }
+    }
+}
